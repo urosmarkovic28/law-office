eb1fe534992cb858eab68af9d698485a1a3bdf83
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-18 17:50:39 +0100

    Services html and css

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php include 'partials/main-header.php' ?>
     <!--Services -->
-    <h1 style="text-align:center; font-size: 10rem;">HOME</h1>
+    <section class="services container">
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Family law</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta mollitia fuga earum, natus reprehenderit incidunt dolores at, quibusdam, quis voluptatem quam a aliquid officiis illum dolorum deserunt cupiditate expedita? Cumque magni earum suscipit culpa et deleniti rerum!</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Business law</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta mollitia fuga earum, natus reprehenderit incidunt dolores at, quibusdam, quis voluptatem quam a aliquid officiis illum dolorum deserunt cupiditate expedita? Cumque magni earum suscipit culpa et deleniti rerum!</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Criminal law</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta mollitia fuga earum, natus reprehenderit incidunt dolores at, quibusdam, quis voluptatem quam a aliquid officiis illum dolorum deserunt cupiditate expedita? Cumque magni earum suscipit culpa et deleniti rerum!</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Insurance law</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta mollitia fuga earum, natus reprehenderit incidunt dolores at, quibusdam, quis voluptatem quam a aliquid officiis illum dolorum deserunt cupiditate expedita? Cumque magni earum suscipit culpa et deleniti rerum!</p>
+            <button>Learn more</button>
+        </article>
+    </section>
 
 <?php include 'partials/footer.php' ?>    
