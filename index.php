eca4277fe3349f5f96bca5503568cbbffd0e0a98
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-18 17:28:51 +0100

    Map

--- index.php
+++ index.php
@@ -54,6 +54,10 @@
             <p>SAT - 11:00/15:00</p>
         </article>
     </section>
-    
+    <!-- MAP -->
+    <section class="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321202.81348918425!2d-114.36801349259746!3d51.027288331159085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C%20AB%2C%20Canada!5e0!3m2!1sen!2srs!4v1616084720009!5m2!1sen!2srs"
+         width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </section>
 </body>
 </html>
