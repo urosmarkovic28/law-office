554d5d6de44381583d92fd781674fccd4fca15ae
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-19 19:05:13 +0100

    About html and css

--- index.php
+++ index.php
@@ -26,5 +26,16 @@
             <button>Learn more</button>
         </article>
     </section>
+    <!-- About -->
+    <section class="about">
+        <article>
+            <h2>About us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate impedit itaque molestias necessitatibus deleniti optio id ex ea voluptates, saepe debitis alias soluta eius nemo, voluptatem quo ipsum voluptate fugit eos incidunt aut assumenda. Non distinctio explicabo consectetur numquam dolores.</p>
+            <a href="#">Learn more &nbsp; &rang;</a>
+        </article>
+        <article>
+            <h2>Welcome to <br> <span>Solo Practice</span> <br> Law</h2>
+        </article>
+    </section>
 
 <?php include 'partials/footer.php' ?>    
